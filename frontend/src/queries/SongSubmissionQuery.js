d6604028a93ef26fb9f6a204b698b5bf54a02e61
Vlpsa <elmeravillalobos@gmail.com>
2023-04-01 14:59:02 -0400

    graphql issues

---
+++ frontend/src/queries/SongSubmissionQuery.js
@@ -0,0 +1,32 @@
+import gql from "graphql-tag";
+export const FETCH_SUBMISSIONS = gql`
+  query FetchUserSongSuggestion {
+    userSongSubmissions(query: {}) {
+      _id
+      username
+      artist
+      song_name
+    }
+  }
+`;
+
+export const ADD_SUBMISSION = gql`
+  mutation AddUserSongSubmission(
+    $username: String!
+    $artist: String!
+    $song_name: String!
+  ) {
+    insertOneUserSongSubmission(
+      data: {
+        username: $username
+        song_name: $song_name
+        artist: $artist
+      }
+    ) {
+        _id
+        username
+        artist
+        song_name
+    }
+  }
+`;
