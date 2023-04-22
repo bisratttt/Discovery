c467d1a5efb7b3c305177b05198c063ad4197902
philipfro <philipfro@vt.edu>
2023-04-22 14:48:43 -0400

    edit only for owner's review

--- frontend/src/components/CommentB.js
+++ frontend/src/components/CommentB.js
@@ -14,6 +14,7 @@ export default function CommentB({ avatar, username, body, title, time }) {
   const [truncateComment, setTruncateComment] = useState(true);
   const [isTruncated, setIsTruncated] = useState(false);
   const { currentUser } = useRealmApp();
+  const owner = username === currentUser.profile.email;
   const commentRef = useRef(null);
   const [isFocused, setIsFocused] = useState(false);
   const [showEditModal, setEditModal] = useState(false);
@@ -93,9 +94,9 @@ export default function CommentB({ avatar, username, body, title, time }) {
                   />
                 </Dropdown.Toggle>
                 <Dropdown.Menu id="review-dropdown">
-                  <Dropdown.Item className="review-dropdown-item" onClick={() => setEditModal(true)}>
+                  {owner && (<Dropdown.Item className="review-dropdown-item" onClick={() => setEditModal(true)}>
                     Edit
-                  </Dropdown.Item>
+                  </Dropdown.Item>)}
                   <Dropdown.Item className="review-dropdown-item">
                     Hide
                   </Dropdown.Item>
